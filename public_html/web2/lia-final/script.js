24823676717224dce1709f8f595ec6e2a929377c
alinaduford <6241958@edu.vaniercollege.qc.ca>
2025-05-09 08:43:25 -0400

    script.js for lia

--- public_html/web2/lia-final/script.js
+++ public_html/web2/lia-final/script.js
@@ -2,35 +2,10 @@
 let btn = document.querySelector("label.bun");
 let nav = document.querySelector("header nav");
 
-// Set the initial state of the nav to hidden
-// nav.style.display = "none";
 
 btn.addEventListener("click", function () {
-    console.log("Button clicked");
+    // console.log("Button clicked");
     nav.classList.toggle("active");
-    // if (nav.style.display === "block") {
-    //     nav.style.display = "none";
-    // } else {
-    //     nav.style.display = "block";
-    // }
+  
 });
 
-// Close the navigation menu when clicking outside of it
-// document.addEventListener("click", function (event) {
-//     // if(nav.classList.contains("active")) {
-//         console.log(event);
-//         // Check if the click was outside the nav and button
-//         if (!nav.contains(event.target) && !btn.contains(event.target) && nav.classList.contains("active")) {
-//         //     nav.style.display = "none";
-//             console.log("Clicked outside");
-//             //nav.classList.toggle("active");
-//         }
-        
-//         // Close the nav
-//         // nav.style.display = "none";
-    
-//     // if (!nav.contains(event.target) && !btn.contains(event.target)) {
-//         // nav.style.display = "none";
-        
-    
-// });
